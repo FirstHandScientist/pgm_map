9e1955a2209039add00ecc67cd9cd49b0dcc5c25
Dong Liu <doli@kth.se>
2018-03-26 13:58:19 +0200

    initialize

---
+++ auto/myReadingMemo.el
@@ -0,0 +1,45 @@
+(TeX-add-style-hook
+ "myReadingMemo"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "fixltx2e"
+    "graphicx"
+    "longtable"
+    "float"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "marvosym"
+    "wasysym"
+    "amssymb"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec-1"
+    "sec-1-1"
+    "sec-1-2"
+    "sec-1-2-1"
+    "sec-1-2-2"
+    "sec-1-2-3"
+    "sec-2")
+   (LaTeX-add-bibliographies
+    "../../../bibliography/mLearningMemo"))
+ :latex)
+
